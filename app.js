39cab653a1f1dca6fc060c8820b5b4676017fd47
ishizuya <takuma3704@gmail.com>
2022-09-19 13:14:34 +0000

    javascript3

--- app.js
+++ app.js
@@ -37,6 +37,7 @@ alert(1-1);
 alert(1*3);
 alert(10/2);
 
+// letによる再宣言
 var nickname = "taro"
 console.log(nickname)
 var nickname = "ichiro"
@@ -47,3 +48,105 @@ var nickname = "taro"
 console.log(nickname)
 nickname = "jiro"
 console.log(nickname)
+
+// var str = "webcamp" //グローバルスコープ
+
+// function foo() {
+//   console.log(str)
+//   var y = "hello" //関数スコープ=ローカルスコープ
+// }
+
+// foo()
+// console.log(y)//→エラーになる、ｙは関数fooの中でしか利用できない
+
+// // var str = "webcamp"
+
+// // function foo() {
+// //   console.log(str)
+// //   var str = "dmm webcamp"
+// //   console.log(str)
+// // }
+
+// foo()
+
+// メイン部分
+let alertString;
+alertString = addString("WebCamp");
+
+// 作成した関数を呼び出す
+alert(alertString);
+
+// 作成した関数
+function addString(strA){
+  let addStr = "Hello " + strA;
+  return addStr;
+}
+
+
+// じゃんけんの手を入力してもらうプロンプト欄を生成
+let user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。');
+//グー、チョキ、パー、null以外の場合「グーチョキパーのいずれかを入力してくださいと表示」して再びじゃんけん入力のプロンプト表示
+while ((user_hand != "グー") && (user_hand != "チョキ") && (user_hand != "パー") && (user_hand != null)){
+  alert('グー・チョキ・パーのいずれかを入力して下さい');
+  user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。');
+}
+// じゃんけんの手をランダムに作成する関数を呼び出す
+let js_hand = getJShand();
+
+// ユーザの手とJavaScriptのじゃんけんの手を比べる関数を呼び出し、結果をjudgeに入れる
+let judge = winLose(user_hand, js_hand);
+
+// もし空欄でなかったら結果表示、nullだったら「またチャレンジしてね」表示
+if (user_hand != null){
+  alert('あなたの選んだ手は' + user_hand + 'です。\nJavaScriptの選んだ手は' + js_hand + 'です。\n結果は' + judge + 'です。');
+} else {
+  alert("またチャレンジしてね")
+}
+// ランダムでじゃんけんの手を作成する関数
+function getJShand(){
+  let js_hand_num = Math.floor( Math.random() * 3 );
+  let hand_name;
+
+  if(js_hand_num == 0){
+    hand_name = "グー";
+  } else if(js_hand_num == 1){
+    hand_name = "チョキ";
+  } else if(js_hand_num == 2){
+    hand_name = "パー";
+  }
+
+  return hand_name;
+}
+
+// ユーザの手とJavaScriptのじゃんけんの手を比べる関数
+function winLose(user, js){
+  let winLoseStr;
+
+  if(user == "グー"){
+    if(js == "グー"){
+      winLoseStr = "あいこ";
+    } else if(js == "チョキ"){
+      winLoseStr = "勝ち";
+    } else if(js == "パー"){
+      winLoseStr = "負け";
+    }
+  } else if(user == "チョキ"){
+    if(js == "グー"){
+      winLoseStr = "負け";
+    } else if(js == "チョキ"){
+      winLoseStr = "あいこ";
+    } else if(js == "パー"){
+      winLoseStr = "勝ち";
+    }
+  } else if(user == "パー"){
+    if(js == "グー"){
+      winLoseStr = "勝ち";
+    } else if(js == "チョキ"){
+      winLoseStr = "負け";
+    } else if(js == "パー"){
+      winLoseStr = "あいこ";
+    }
+  }
+
+  return winLoseStr;
+}
